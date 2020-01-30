8f741f7af804fdaaeee5ad781268b2a132b497e8
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-01-30 08:49:39 -0500

    Wrote method for bin2decimal

--- src/main/java/MidTermSecondAttempt.java
+++ src/main/java/MidTermSecondAttempt.java
@@ -5,12 +5,14 @@ public class MidTermSecondAttempt {
         String[] main = new String[]{"Weeeeee are the champions"};
         String main2 = "WWWWWWWWWWWWWWWWWWWWWWWWWWee are the champions";
         MidTermSecondAttempt obj1 = new MidTermSecondAttempt();
-       // for (int i = 0; i < main.length; i++) {
-            //System.out.println(obj1.reverseSentence(main[i]));
-       // }
-        System.out.println(obj1.mostFrequent(main2));
+        // for (int i = 0; i < main.length; i++) {
+        //System.out.println(obj1.reverseSentence(main[i]));
+        // }
+        //System.out.println(obj1.mostFrequent(main2));
+        System.out.println(binaryToDecimal("1000"));
     }
-        public String reverseSentence (String s){
+
+    public String reverseSentence(String s) {
            /* //ArrayList<String> newWords= new ArrayList<>();
             char[] t = s.toCharArray();
             String newWords = "";
@@ -29,30 +31,46 @@ public class MidTermSecondAttempt {
             }
             return newWords; */
 
-           String[] newWords = s.split(" ");
-           String finalString = "";
-           for(int i = newWords.length-1; i >=0; i--){
-               finalString += " " + newWords[i];
-           }
-           return finalString;
+        String[] newWords = s.split(" ");
+        String finalString = "";
+        for (int i = newWords.length - 1; i >= 0; i--) {
+            finalString += " " + newWords[i];
         }
+        return finalString;
+    }
 
-        public char mostFrequent(String s){
+    public char mostFrequent(String s) {
         char[] t = s.toCharArray();
         int i = 0;
         int[] count = new int[200];
         int max = 0;
-        for(i =0; i<=t.length; i++){
-            for(int j =i+1; j<t.length; j++){
-                if (t[i] == t[j]){
+        for (i = 0; i <= t.length; i++) {
+            for (int j = i + 1; j < t.length; j++) {
+                if (t[i] == t[j]) {
                     max = count[i];
                     count[i]++;
-                    if(count[i] > max){
+                    if (count[i] > max) {
                         max = count[i];
                     }
                 }
             }
         }
         return s.charAt(max);
-        }
     }
+
+    public static int binaryToDecimal(String s) {
+        if (s == null) {
+            return 0;
+        }
+        char[] binNumbers = s.toCharArray();
+        int power = 0;
+        int result = 0;
+        for (int i = binNumbers.length - 1; i >= 0; i--) {
+            int charValue = Integer.parseInt(s.valueOf(binNumbers[i]));
+            result += charValue * Math.pow(2, power);
+            power++;
+        }
+        return result;
+
+    }
+}
